0d0cbe932b863873201ab065cf04fca3f37a26d3
Greg Anderson <glanderson5@gmail.com>
2022-08-04 17:06:51 -0500

    Added createGrid function

--- scripts/etch.js
+++ scripts/etch.js
@@ -1,5 +1,14 @@
 let size = 16;
+createGrid(size);
 
 function createGrid(size) {
-    
+    const grid = document.querySelector(".grid");
+
+    for (let i = 0; i < size * size; i++) {
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        grid.appendChild(cell);
+    }
+
+    grid.setAttribute("style", `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`);
 }
