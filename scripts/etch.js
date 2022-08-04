0ffe7cc8d8d6038ff4917cac05709f6f6c8d6c92
Greg Anderson <glanderson5@gmail.com>
2022-08-04 17:13:23 -0500

    Cells are now shaded upon mouseover

--- scripts/etch.js
+++ scripts/etch.js
@@ -7,8 +7,12 @@ function createGrid(size) {
     for (let i = 0; i < size * size; i++) {
         let cell = document.createElement("div");
         cell.classList.add("cell");
+        cell.addEventListener("mouseover", () => {
+            cell.style.cssText = "background-color: black";
+        });
         grid.appendChild(cell);
     }
 
     grid.setAttribute("style", `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`);
 }
+
