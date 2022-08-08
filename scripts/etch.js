4d5ac23a838b265c6813301f098becc793bafb1b
Greg Anderson <glanderson5@gmail.com>
2022-08-08 16:27:01 -0500

    Added slider that adjust size of grid

--- scripts/etch.js
+++ scripts/etch.js
@@ -1,14 +1,22 @@
 let size = 16;
+
 const clearButton = document.querySelector("#clear");
 clearButton.addEventListener("click", () => {
     clearGrid();
 });
 
+// const sizeSlider = document.querySelector(".sizeSlider");
+// sizeSlider.addEventListener("input", () => {
+//     size = this.value;
+//     createGrid(size);
+//     console.log(size);
+// });
 
-function createGrid(size) {
+
+function createGrid(n) {
     const grid = document.querySelector(".grid");
 
-    for (let i = 0; i < size * size; i++) {
+    for (let i = 0; i < n * n; i++) {
         let cell = document.createElement("div");
         cell.classList.add("cell");
         cell.addEventListener("mouseover", () => {
@@ -17,13 +25,19 @@ function createGrid(size) {
         grid.appendChild(cell);
     }
 
-    grid.setAttribute("style", `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`);
+    grid.setAttribute("style", `grid-template-columns: repeat(${n}, 1fr); grid-template-rows: repeat(${n}, 1fr);`);
 }
 
 function clearGrid() {
     let cells = document.querySelectorAll("div.cell");
-    cells.forEach(cell => cell.style.cssText = "background-color: white");
-    console.log("why?");
+    cells.forEach(cell => cell.remove());
+    createGrid(size);
+}
+
+function refreshGrid(n) {
+    size = n;
+    clearGrid();
+    
 }
 
 createGrid(size);
