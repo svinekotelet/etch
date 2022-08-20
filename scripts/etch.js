cfc34e378f183d5a9dedc88a970fab85d6e2b8d2
Greg Anderson <glanderson5@gmail.com>
2022-08-19 21:40:53 -0500

    ADded functionality to greyscale color setting

--- scripts/etch.js
+++ scripts/etch.js
@@ -1,5 +1,7 @@
 let size = 16;
 let color = "black"
+let greyMode = false;
+let rainbowMode = false;
 let borderType = "solid";
 
 createGrid(size);
@@ -15,6 +17,25 @@ border.addEventListener("change", () => {
     clearGrid();
 });
 
+const colorSetting = document.querySelector(".colorForm");
+colorSetting.addEventListener("change", () => {
+    switch (document.forms.colorForm.colorMode.value) {
+        case "black": 
+            greyMode = false;
+            rainbowMode = false;
+            break;
+        case "grey":
+            greyMode = true;
+            rainbowMode = false;
+            break;
+        case "rainbow":
+            greyMode = false;
+            rainbowMode = true;                
+            break;
+    }
+    clearGrid();
+});
+
 function createGrid(n) {
     const grid = document.querySelector(".grid");
 
@@ -22,7 +43,14 @@ function createGrid(n) {
         let cell = document.createElement("div");
         cell.classList.add("cell");
         cell.style.borderStyle = borderType;
+        if (greyMode) {
+            cell.style.opacity = 0;
+            cell.style.borderColor = rgba(0,0,0,1);
+        }
         cell.addEventListener("mouseover", () => {
+            if (greyMode) {
+                cell.style.opacity = (parseInt(cell.style.opacity * 10) + 2) / 10;
+            }
             cell.style.backgroundColor = color;
         });
         grid.appendChild(cell);
